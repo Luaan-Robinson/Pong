7ff7f7fce54957a30354faf9a38141f765deef8b
Luaan-Robinson <106935625+Luaan-Robinson@users.noreply.github.com>
2026-04-20 14:59:19 +0200

    CPU movement and score

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
 from settings import * 
 from sprites import *
+import json
+from groups import *
 
 class Game:
     def __init__(self):
@@ -10,10 +12,36 @@ class Game:
         self.running = True
 
         # sprites
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = AllSprites()
         self.paddle_sprites = pygame.sprite.Group() # used for collision between ball and paddles
         self.player = Player((self.all_sprites, self.paddle_sprites))
-        self.ball = Ball(self.all_sprites, self.paddle_sprites) # ball has access to paddle_sprites but is NOT in paddle_sprites
+        self.ball = Ball(self.all_sprites, self.paddle_sprites, self.update_score) # ball has access to paddle_sprites but is NOT in paddle_sprites
+        Opponent((self.all_sprites, self.paddle_sprites), self.ball)
+
+        # score
+        try:
+            with open(join('data', 'score.txt')) as score_file:
+                self.score = json.load(score_file)
+        except:    
+            self.score = {'player': 0, 'opponent': 0}
+        self.font = pygame.font.Font(None, 160) 
+        
+    def display_score(self):
+        #player
+        player_surf = self.font.render(str(self.score['player']), True, COLORS['bg detail'])
+        player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2 + 100, WINDOW_HEIGHT / 2))
+        self.display_surface.blit(player_surf, player_rect)
+
+        # opponent    
+        opponent_surf = self.font.render(str(self.score['opponent']), True, COLORS['bg detail'])
+        opponent_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 2))
+        self.display_surface.blit(opponent_surf, opponent_rect)
+
+        #  line separator
+        pygame.draw.line(self.display_surface, COLORS['bg detail'], (WINDOW_WIDTH / 2, 0), (WINDOW_WIDTH / 2, WINDOW_HEIGHT), 6)
+
+    def update_score(self, side):
+        self.score['player' if side == 'player' else 'opponent'] += 1
 
     def run(self):
         while self.running:
@@ -21,13 +49,16 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                    with open(join('data', 'score.txt'), 'w') as score_file:
+                        json.dump(self.score, score_file)
 
             # update 
             self.all_sprites.update(dt)
             
             # draw     
             self.display_surface.fill(COLORS['bg'])       
-            self.all_sprites.draw(self.display_surface)
+            self.display_score()
+            self.all_sprites.draw()
             pygame.display.update()
         pygame.quit() # remember to call this after the self.running while loop   
 
--- code/sprites.py
+++ code/sprites.py
@@ -1,7 +1,7 @@
 from settings import *
 from random import choice, uniform
 
-class Player(pygame.sprite.Sprite):
+class Paddle(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(groups)
 
@@ -9,44 +9,98 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface(SIZE['paddle'], pygame.SRCALPHA)
         pygame.draw.rect(self.image, COLORS['paddle'], pygame.FRect((0,0), (SIZE['paddle'])), 0, 6)
       
-
+        # shadow surf
+        self.shadow_surf = self.image.copy()
+        pygame.draw.rect(self.shadow_surf, COLORS['paddle shadow'], pygame.FRect((0,0), (SIZE['paddle'])), 0, 6)
 
         # rect & movement
         self.rect = self.image.get_frect(center = POS['player'])
+        self.old_rect = self.rect.copy()
         self.direction = 0
-        self.speed = SPEED['player']
 
     def move(self, dt):
         self.rect.centery += self.direction * self.speed * dt
         self.rect.top = 0 if self.rect.top < 0 else self.rect.top
         self.rect.bottom = WINDOW_HEIGHT if self.rect.bottom > WINDOW_HEIGHT else self.rect.bottom
 
+    def update(self, dt):
+        self.old_rect = self.rect.copy()
+        self.get_direction()
+        self.move(dt)     
+        
+class Player(Paddle):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.speed = SPEED['player']
+
     def get_direction(self):
         keys = pygame.key.get_pressed()
         self.direction = int(keys[pygame.K_DOWN] - int(keys[pygame.K_UP]))  
     
 
-    def update(self, dt):
-        self.get_direction()
-        self.move(dt)      
-
-class Ball(pygame.sprite.Sprite)  :
-    def __init__(self, groups, paddle_sprites):
+class Opponent(Paddle):
+    def __init__(self, groups, ball):
         super().__init__(groups)
+        self.speed = SPEED['opponent']
+        self.rect.center = POS['opponent']
+        self.ball = ball
+
+
+    def get_direction(self):
+        self.direction = 1 if self.ball.rect.centery > self.rect.centery else -1 # I don't know how this works, but it does
+
+# Elite Ball Knowledge
+class Ball(pygame.sprite.Sprite)  :
+    def __init__(self, groups, paddle_sprites, update_score):
+        super().__init__(groups)
+        self.paddle_sprites = paddle_sprites
+        self.update_score = update_score
 
         # image
         self.image = pygame.Surface(SIZE['ball'], pygame.SRCALPHA)
         pygame.draw.circle(self.image, COLORS['ball'], (SIZE['ball'][0] / 2, SIZE['ball'][1] / 2), SIZE['ball'][0] / 2)
         
+        # shadow surf
+        self.shadow_surf = self.image.copy()
+        pygame.draw.circle(self.shadow_surf, COLORS['ball shadow'], (SIZE['ball'][0] / 2, SIZE['ball'][1] / 2), SIZE['ball'][0] / 2)
 
         
-        # rect & movement      
+        # ball & movement (elite ball knowledge)      
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-
+        self.old_rect = self.rect.copy()
         self.direction = pygame.Vector2(choice((1,-1)), uniform(0.7, 0.8) * choice((-1, 1))) # man, I don't even know myself
+        self.speed_modifier = 0
+
+        # timer
+        self.start_time = pygame.time.get_ticks()
+        self.duration = 1200
 
     def move(self, dt):
-        self.rect.center += self.direction * SPEED['ball'] * dt
+        self.rect.x += self.direction.x * SPEED['ball'] * dt * self.speed_modifier
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * SPEED['ball'] * dt * self.speed_modifier
+        self.collision('vertical')
+        
+
+
+    def collision(self, direction):
+        for sprite in self.paddle_sprites:
+            if sprite.rect.colliderect(self.rect):  # checking if player and ball collide
+                if direction == 'horizontal':
+                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                        self.rect.right = sprite.rect.left
+                        self.direction.x *= -1
+                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                        self.rect.left = sprite.rect.right
+                        self.direction.x *= -1     
+
+                else:
+                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top: # checks if ball is top of paddle
+                        self.rect.bottom = sprite.rect.top 
+                        self.direction.y *= -1    
+                    if self.rect.top <= sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                        self.rect.top = sprite.rect.bottom
+                        self.direction.y *= -1       
 
     def wall_collision(self):
         if self.rect.top <= 0:
@@ -57,18 +111,25 @@ class Ball(pygame.sprite.Sprite)  :
             self.rect.bottom = WINDOW_HEIGHT
             self.direction.y *= -1     
 
-        # temporary side bounce for testing
-        if self.rect.right >= WINDOW_WIDTH:
-            self.rect.right = WINDOW_WIDTH
-            self.direction.x *= -1   
+        if self.rect.right >= WINDOW_WIDTH or self.rect.left <= 0:
+            self.update_score('player' if self.rect.x < WINDOW_WIDTH / 2 else 'opponent')
+            self.reset()
 
-        if self.rect.left <= 0:
-            self.rect.left = 0
-            self.direction.x *= -1        
-
-
+    def reset(self):
+        self.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.direction = pygame.Vector2(choice((1,-1)), uniform(0.7, 0.8) * choice((-1, 1)))
+        self.start_time = pygame.time.get_ticks()
+        
+    def timer(self):
+        if pygame.time.get_ticks() - self.start_time >= self.duration:
+            self.speed_modifier = 1
+        else:
+            self.speed_modifier = 0    
 
 
     def update(self, dt):
-        self.move(dt)        
+        self.old_rect = self.rect.copy() # previous frame
+        self.timer()
+        self.move(dt)                    # current frame
         self.wall_collision()
+ 
